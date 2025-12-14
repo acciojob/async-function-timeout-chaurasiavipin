fc0e54a0392483f06b477c7513ca7c00c6779d49
chaurasiavipin <vipinchaurasia481@gmail.com>
2025-12-14 22:57:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const input=document.getElementById("text");
+const delayTime=document.getElementById("delay");
+const submit=document.getElementById("btn");
+const div=document.getElementById("output");
+function delay(ms) {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms);
+    });
+}
+submit.addEventListener('click', async function(){
+	const store=Number(delayTime.value)*1000;
+	
+
+await delay(store);
+const newline=document.creatElement('p')
+newline.textContent=input.value;
+	
+div.appendChild(newline)
+
+});
+
+
+
+
+	
